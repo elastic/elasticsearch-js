90559ead064845e295496c357ccaa20dd650256d
Jeff French <jeff.french@moonswitch.com>
2014-02-13 20:02:40 -0600

    User Buffer for base64 encoding and remove withCredentials.

--- src/lib/connectors/angular.js
+++ src/lib/connectors/angular.js
@@ -9,30 +9,26 @@ module.exports = AngularConnector;
 var _ = require('../utils');
 var ConnectionAbstract = require('../connection');
 var ConnectionFault = require('../errors').ConnectionFault;
-var base64 = require('js-base64').Base64;
 
 function AngularConnector(host, config) {
   ConnectionAbstract.call(this, host, config);
   this.defer = config.defer;
   this.$http = config.$http;
+  if(this.host.auth) {
+    this.$http.defaults.headers.common.Authorization = 'Basic ' + Buffer(this.host.auth, 'utf8').toString('base64');
+  }
 }
 _.inherits(AngularConnector, ConnectionAbstract);
 
 AngularConnector.prototype.request = function (params, cb) {
   var abort = this.defer();
-  var headers = {};
-  if(this.host.auth) {
-    headers['Authorization'] = 'Basic ' + base64.encode(this.host.auth);
-  }
   this.$http({
     method: params.method,
     url: this.host.makeUrl(params),
     data: params.body,
     cache: false,
     transformRequest: [],
-    transformResponse: [],
-    headers: headers,
-    withCredentials: !!this.host.auth
+    transformResponse: []
   }).then(function (response) {
     cb(null, response.data, response.status, response.headers());
   }, function (err) {
