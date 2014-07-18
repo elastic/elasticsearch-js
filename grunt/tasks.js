83ecd4914cf8cefee684bda79a8743452acd0fc5
Spencer Alger <spencer@spenceralger.com>
2014-07-18 04:41:09 -0700

    added version upgrade task

--- grunt/tasks.js
+++ grunt/tasks.js
@@ -1,5 +1,9 @@
 module.exports = function (grunt) {
+  var Promise = require('bluebird');
   var utils = require('./utils');
+  var readFile = Promise.promisify(require('fs').readFile);
+  var writeFile = Promise.promisify(require('fs').writeFile);
+
 
   // Default task runs the build process.
   grunt.registerTask('default', [
@@ -37,4 +41,37 @@ module.exports = function (grunt) {
     'mochacov:make_coverage_html',
     'open:coverage'
   ]);
+
+  grunt.registerTask('version', function (type) {
+    var root = require('path').join.bind(null, __dirname, '..');
+    var readmePath = root('README.md');
+    var packagePath = root('package.json');
+    var browserBuildsPath = root('docs/browser_builds.asciidoc');
+
+    Promise.all([
+      require(packagePath),
+      readFile(readmePath, 'utf8'),
+      readFile(browserBuildsPath, 'utf8')
+    ])
+    .spread(function (pkg, readme, browserBuilds) {
+      var current = pkg.version;
+      var next = utils.increaseVersion(current, type);
+
+      pkg.version = next;
+      browserBuilds = utils.replaceAll(browserBuilds, current, next);
+      readme = utils.replaceAll(readme, current, next);
+      readme = utils.replaceAll(
+        readme,
+        'branch=' + utils.minorVersion(current),
+        'branch=' + utils.minorVersion(next)
+      );
+
+      // write all files to disk
+      return Promise.all([
+        writeFile(readmePath, readme),
+        writeFile(browserBuildsPath, browserBuilds),
+        writeFile(packagePath, JSON.stringify(pkg, null, '  '))
+      ]);
+    }).nodeify(this.async());
+  });
 };
