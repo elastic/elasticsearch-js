153c8e381207b4aefeedc96d2459890bbbfc3701
Spencer Alger <spencer@spenceralger.com>
2014-07-18 04:41:09 -0700

    added version upgrade task

--- grunt/utils.js
+++ grunt/utils.js
@@ -16,4 +16,61 @@ var utils = {
 
 utils.branches._default = pkg.config.default_api_branch;
 
+/*
+ * trim down a version id to the minor version number ('1.5.1' => '1.5')
+ */
+utils.minorVersion = function (version) {
+  return version.split('.').slice(0, 2).join('.');
+};
+
+/*
+ * increment the version based on the release "type"
+ */
+utils.increaseVersion = function (version, type) {
+  var i;
+  switch (type) {
+  case 'major':
+    i = 0;
+    break;
+  case 'minor':
+    i = 1;
+    break;
+  case 'bug':
+  case 'patch':
+  case 'bugfix':
+    i = 2;
+    break;
+  default:
+    throw new TypeError('unexpected version bump type');
+  }
+
+  // breakout the current version
+  var next = version.split('.').map(function (n) {
+    return parseInt(n, 10);
+  });
+
+  // increment the version type
+  next[i] += 1;
+  // clear out all following numbers
+  for (i ++; i < next.length; i++) next[i] = 0;
+  // join back together with '.'
+  return next.join('.');
+};
+
+/*
+ * replace all instances of `replace` with `replacement` without creating a regexp object
+ */
+utils.replaceAll = function (str, replace, replacement) {
+  var out = '';
+  var remaining = str;
+  var i = 0;
+
+  while (~(i = remaining.indexOf(replace))) {
+    out += remaining.substring(0, i) + replacement;
+    remaining = remaining.substr(i + replace.length);
+  }
+
+  return out + remaining;
+};
+
 module.exports = utils;
