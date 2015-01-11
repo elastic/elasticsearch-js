945f26a38bc293ebfad9e2d2b19284f21502b0d3
Spencer Alger <spencer@spenceralger.com>
2015-01-11 00:48:01 -0700

    update jenkins config

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -54,4 +54,4 @@ install_node "$NODE_V"
 npm install
 release_lock
 
-ES_PORT=$((9400 + EXECUTOR_NUMBER)) node ./scripts/ci.js
+ES_PORT=$((9400 + EXECUTOR_NUMBER)) RUN=NODE_UNIT,NODE_INTEGRATION node ./scripts/ci.js
