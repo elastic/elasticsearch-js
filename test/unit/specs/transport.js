ae69f624a72cedc2e4d025c5034c277ff5e6279a
Spencer Alger <email@spalger.com>
2015-03-26 13:55:25 -0700

    added timer stubs for clear, since not all platforms accept a number in clearTimeout

--- test/unit/specs/transport.js
+++ test/unit/specs/transport.js
@@ -627,7 +627,7 @@ describe('Transport Class', function () {
 
     describe('timeout', function () {
       it('uses 30 seconds for the default', function () {
-        var clock = sinon.useFakeTimers('setTimeout');
+        var clock = sinon.useFakeTimers('setTimeout', 'clearTimeout');
         stub.autoRelease(clock);
         var tran = new Transport({});
         var err;
@@ -643,7 +643,7 @@ describe('Transport Class', function () {
         });
       });
       it('inherits the requestTimeout from the transport', function () {
-        var clock = sinon.useFakeTimers('setTimeout');
+        var clock = sinon.useFakeTimers('setTimeout', 'clearTimeout');
         stub.autoRelease(clock);
         var tran = new Transport({
           requestTimeout: 5000
