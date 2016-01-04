f10bcfcd45f015ad3c3b5cf002ecba76f3f2fc72
spalger <spalger@users.noreply.github.com>
2016-01-04 10:44:34 -0700

    added 2.2 apiVersion

--- src/lib/apis/browser_index.js
+++ src/lib/apis/browser_index.js
@@ -1,8 +1,8 @@
 module.exports = {
-  '_default': require('./2_1'),
+  '_default': require('./2_2'),
+  '2.2': require('./2_2'),
   '2.1': require('./2_1'),
   '2.0': require('./2_0'),
   '1.7': require('./1_7'),
-  '1.6': require('./1_6'),
-  '1.5': require('./1_5')
+  '1.6': require('./1_6')
 };
--- src/lib/apis/index.js
+++ src/lib/apis/index.js
@@ -1,5 +1,6 @@
 module.exports = {
-  '_default': require('./2_1'),
+  '_default': require('./2_2'),
+  '2.2': require('./2_2'),
   '2.1': require('./2_1'),
   '2.0': require('./2_0'),
   '1.7': require('./1_7'),
