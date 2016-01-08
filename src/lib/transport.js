57238355a4e85991d98bd54f1e95f1e500a5aee1
Jesse McCarthy <git_commits@jessemccarthy.net>
2016-01-08 13:42:49 -0500

    Enhance error message with more detail.

--- src/lib/transport.js
+++ src/lib/transport.js
@@ -226,12 +226,24 @@ Transport.prototype.request = function (params, cb) {
 
     if (err) {
       connection.setStatus('dead');
+
+      var errMsg = err.message || '';
+
+      errMsg =
+        "\n" +
+        params.req.method +
+        ' ' +
+        connection.host.makeUrl(params.req) +
+        (errMsg.length ? ' => ' : '') +
+        errMsg
+      ;
+
       if (remainingRetries) {
         remainingRetries--;
-        self.log.error('Request error, retrying' + (err.message ? ' -- ' + err.message : ''));
+        self.log.error('Request error, retrying' + errMsg);
         self.connectionPool.select(sendReqWithConnection);
       } else {
-        self.log.error('Request complete with error' + (err.message ? ' -- ' + err.message : ''));
+        self.log.error('Request complete with error' + errMsg);
         respond(new errors.ConnectionFault(err));
       }
     } else {
