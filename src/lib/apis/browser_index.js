50898d3fc5fcce6a894a5d1dfee71dd819954ddd
spalger <spalger@users.noreply.github.com>
2017-06-12 10:54:15 -0700

    change default api branch to 5.4

--- src/lib/apis/browser_index.js
+++ src/lib/apis/browser_index.js
@@ -1,6 +1,6 @@
 module.exports = {
+  '_default': require('./5_4'),
   '5.4': require('./5_4'),
-  '_default': require('./5_3'),
   '5.3': require('./5_3'),
   '5.2': require('./5_2'),
   '5.1': require('./5_1'),
