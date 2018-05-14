0f2b5f79774db879c8482b11bf9dbb8c41254188
spalger <spalger@users.noreply.github.com>
2018-05-14 12:35:11 -0700

    [aws] remove support for aws-config files

--- grunt/config/aws_s3.js
+++ grunt/config/aws_s3.js
@@ -1,9 +1,3 @@
-var config = {};
-try {
-  // eslint-disable-next-line
-  config = require('../../.aws-config.json');
-} catch (e) {}
-
 module.exports = {
   upload_archives: {
     files: [
@@ -15,8 +9,8 @@ module.exports = {
       },
     ],
     options: {
-      accessKeyId: config.key || process.env.AWS_KEY,
-      secretAccessKey: config.secret || process.env.AWS_SECRET,
+      accessKeyId: process.env.AWS_KEY,
+      secretAccessKey: process.env.AWS_SECRET,
       bucket: 'download.elasticsearch.org',
       access: 'public-read',
       params: {
