5bca9a4bef889f186d9197fc16fcaa7453d79352
delvedor <tommydelved@gmail.com>
2018-12-21 17:52:37 +0100

    Updated scripts to support request params type generation

--- scripts/utils/index.js
+++ scripts/utils/index.js
@@ -1,7 +1,8 @@
 'use strict'
 
 const generate = require('./generate')
+const generateRequestTypes = require('./generateRequestTypes')
 const cloneAndCheckout = require('./clone-es')
 const genFactory = require('./genMain')
 
-module.exports = { generate, cloneAndCheckout, genFactory }
+module.exports = { generate, cloneAndCheckout, genFactory, generateRequestTypes }
