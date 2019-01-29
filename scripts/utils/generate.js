2d14e2c8430fb6a46cae9bd4ee113f850110c7ac
delvedor <tommydelved@gmail.com>
2019-01-29 12:55:35 +0100

    Updated code generation

--- scripts/utils/generate.js
+++ scripts/utils/generate.js
@@ -95,6 +95,7 @@ function generate (spec, common) {
 
     ${genUrlValidation(paths, api)}
 
+    var warnings = null
     // build querystring object
     const querystring = {}
     const keys = Object.keys(params)
@@ -104,15 +105,22 @@ function generate (spec, common) {
     const acceptedQuerystringCamelCased = [
       ${genAcceptedQuerystringCamelCased()}
     ]
+    const queryBlacklist = [
+      ${genQueryBlacklist()}
+    ]
 
     for (var i = 0, len = keys.length; i < len; i++) {
       var key = keys[i]
-      if (acceptedQuerystring.indexOf(key) !== -1) {
-        querystring[key] = params[key]
+      var camelIndex = acceptedQuerystringCamelCased.indexOf(key)
+      if (camelIndex !== -1) {
+        querystring[acceptedQuerystring[camelIndex]] = params[key]
       } else {
-        var camelIndex = acceptedQuerystringCamelCased.indexOf(key)
-        if (camelIndex !== -1) {
-          querystring[acceptedQuerystring[camelIndex]] = params[key]
+        if (acceptedQuerystring.indexOf(key) !== -1) {
+          querystring[key] = params[key]
+        } else if (queryBlacklist.indexOf(key) === -1) {
+          warnings = warnings || []
+          warnings.push('Client - Unknown parameter: "' + key + '", sending it as query parameter')
+          querystring[key] = params[key]
         }
       }
     }
@@ -153,7 +161,8 @@ function generate (spec, common) {
       requestTimeout: options.requestTimeout || null,
       maxRetries: options.maxRetries || null,
       asStream: options.asStream || false,
-      headers: options.headers || null
+      headers: options.headers || null,
+      warnings
     }
 
     return makeRequest(request, requestOptions, callback)
@@ -246,6 +255,14 @@ function generate (spec, common) {
       })
       .map(q => `'${q}'`)
       .join(',\n          ')
+  }
+
+  function genQueryBlacklist () {
+    const blacklist = [`'method'`, `'body'`]
+    if (typeof parts === 'object' && parts !== null) {
+      Object.keys(parts).forEach(p => blacklist.push(`'${p}'`))
+    }
+    return blacklist
   }
 
   function buildPath () {
