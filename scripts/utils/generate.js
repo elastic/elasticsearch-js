1b519671eb431a4b914bc789d307672cf7488f32
delvedor <tommydelved@gmail.com>
2019-02-12 16:39:42 +0100

    Updated code generation

--- scripts/utils/generate.js
+++ scripts/utils/generate.js
@@ -135,6 +135,7 @@ function generate (spec, common) {
       maxRetries: options.maxRetries || null,
       asStream: options.asStream || false,
       headers: options.headers || null,
+      compression: options.compression || false,
       warnings
     }
 
