5a18409a6132c6dd86d0b553f637d61b73e5bddd
delvedor <tommydelved@gmail.com>
2019-02-14 07:57:16 +0100

    Added documentation generator

--- scripts/utils/index.js
+++ scripts/utils/index.js
@@ -4,5 +4,12 @@ const generate = require('./generate')
 const generateRequestTypes = require('./generateRequestTypes')
 const cloneAndCheckout = require('./clone-es')
 const genFactory = require('./genMain')
+const generateDocs = require('./generateDocs')
 
-module.exports = { generate, cloneAndCheckout, genFactory, generateRequestTypes }
+module.exports = {
+  generate,
+  cloneAndCheckout,
+  genFactory,
+  generateRequestTypes,
+  generateDocs
+}
