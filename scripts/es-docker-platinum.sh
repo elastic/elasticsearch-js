27980240314b541b7415bdb64d382290b953dfb9
Tomas Della Vedova <delvedor@users.noreply.github.com>
2019-07-10 15:27:44 +0200

    Improve integration test (#859)
    * CI: Added junit plugin
    
    * Updated .gitignore
    
    * Added integration test reporter
    
    * Updated integration testing suite
    
    * Updated ci config
    
    * Updated report file path
    
    * Use refresh 'true' instead of 'wait_for'
    
    * Disable junit reporting
    
    * Refresh one single time
    
    * Update security index name
    
    * Updated skip test handling and use class syntax
    
    * Updated test script
    
    * Disable test timeout
    
    * Added command to automatically remove an old snapshot
    
    * Disable timeout in integration test script
    
    * Updated logs and cleaned up git handling
    
    * Fixed shouldSkip utility
    
    * Updated cleanup code
    
    * Updated cleanup code pt 2
    
    * Rename Platinum to XPack

--- scripts/es-docker-platinum.sh
+++ scripts/es-docker-platinum.sh
@@ -5,6 +5,11 @@ testnodecrt="/.ci/certs/testnode.crt"
 testnodekey="/.ci/certs/testnode.key"
 cacrt="/.ci/certs/ca.crt"
 
+# pass `--clean` to reemove the old snapshot
+if [ "$1" != "" ]; then
+  docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' | grep '8.0.0-SNAPSHOT')
+fi
+
 exec docker run \
   --rm \
   -e "node.attr.testattr=test" \
