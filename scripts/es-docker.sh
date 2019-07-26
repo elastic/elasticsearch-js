19734328bce7bae536d5033a858a25428be7f3b5
delvedor <tommydelved@gmail.com>
2019-07-26 16:55:26 +0200

    Updated es docker scripts

--- scripts/es-docker.sh
+++ scripts/es-docker.sh
@@ -5,17 +5,27 @@
 # the latest snapshot.
 
 # pass `--clean` to reemove the old snapshot
-if [ "$1" != "" ]; then
+if [ "$1" == "--clean" ]; then
   docker rmi $(docker images --format '{{.Repository}}:{{.Tag}}' | grep '8.0.0-SNAPSHOT')
 fi
 
-exec docker run \
-  --rm \
-  -e "node.attr.testattr=test" \
-  -e "path.repo=/tmp" \
-  -e "repositories.url.allowed_urls=http://snapshot.*" \
-  -e "discovery.type=single-node" \
-  -p 9200:9200 \
-  --network=elastic \
-  --name=elasticsearch \
-  docker.elastic.co/elasticsearch/elasticsearch:8.0.0-SNAPSHOT
+if [ "$1" == "--detach" ]; then
+  exec docker run \
+    --rm \
+    -e "node.attr.testattr=test" \
+    -e "path.repo=/tmp" \
+    -e "repositories.url.allowed_urls=http://snapshot.*" \
+    -e "discovery.type=single-node" \
+    -p 9200:9200 \
+    --detach \
+    docker.elastic.co/elasticsearch/elasticsearch:8.0.0-SNAPSHOT
+else
+  exec docker run \
+    --rm \
+    -e "node.attr.testattr=test" \
+    -e "path.repo=/tmp" \
+    -e "repositories.url.allowed_urls=http://snapshot.*" \
+    -e "discovery.type=single-node" \
+    -p 9200:9200 \
+    docker.elastic.co/elasticsearch/elasticsearch:8.0.0-SNAPSHOT
+fi
