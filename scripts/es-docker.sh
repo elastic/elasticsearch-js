dfc0556c4be9d3082f2a0ff9b6ce00fbdd086263
delvedor <tommydelved@gmail.com>
2019-08-09 12:32:43 +0200

    Fixed es-docker.sh

--- scripts/es-docker.sh
+++ scripts/es-docker.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-exec docker run \
-  --rm \
-  -e "node.attr.testattr=test" \
-  -e "path.repo=/tmp" \
-  -e "repositories.url.allowed_urls=http://snapshot.*" \
-  -e "discovery.zen.ping.unicast.hosts=elasticsearch"  \
-  -e "xpack.security.enabled=false" \
-  -e "xpack.monitoring.enabled=false" \
-  -e "xpack.ml.enabled=false" \
-  -p 9200:9200 \
-  --network=elastic \
-  --name=elasticsearch \
-  docker.elastic.co/elasticsearch/elasticsearch:5.6.16
-=======
 # Images are cached locally, it may be needed
 # to delete an old image and download again
 # the latest snapshot.
@@ -33,22 +18,27 @@ if [ "$1" == "--detach" ]; then
     -e "node.attr.testattr=test" \
     -e "path.repo=/tmp" \
     -e "repositories.url.allowed_urls=http://snapshot.*" \
-    -e "discovery.type=single-node" \
+    -e "discovery.zen.ping.unicast.hosts=elasticsearch"  \
+    -e "xpack.security.enabled=false" \
+    -e "xpack.monitoring.enabled=false" \
+    -e "xpack.ml.enabled=false" \
     -p 9200:9200 \
     --detach \
     --network=elastic \
     --name=elasticsearch \
-    docker.elastic.co/elasticsearch/elasticsearch:8.0.0-SNAPSHOT
+    docker.elastic.co/elasticsearch/elasticsearch:5.6.16
 else
   exec docker run \
     --rm \
     -e "node.attr.testattr=test" \
     -e "path.repo=/tmp" \
     -e "repositories.url.allowed_urls=http://snapshot.*" \
-    -e "discovery.type=single-node" \
+    -e "discovery.zen.ping.unicast.hosts=elasticsearch"  \
+    -e "xpack.security.enabled=false" \
+    -e "xpack.monitoring.enabled=false" \
+    -e "xpack.ml.enabled=false" \
     -p 9200:9200 \
     --network=elastic \
     --name=elasticsearch \
-    docker.elastic.co/elasticsearch/elasticsearch:8.0.0-SNAPSHOT
+    docker.elastic.co/elasticsearch/elasticsearch:5.6.16
 fi
->>>>>>> 34e440b9... Use Jenkins pipelines (#937)
