f75fff6b5a341e297023c77e6ab1b93377e2b248
spalger <email@spalger.com>
2019-09-18 18:31:25 -0700

    [api] regenerate

--- src/lib/apis/browser_index.js
+++ src/lib/apis/browser_index.js
@@ -1,11 +1,10 @@
 module.exports = {
-  '_default': require('./7_2'),
+  '_default': require('./7_3'),
+  '7.3': require('./7_3'),
   '7.2': require('./7_2'),
   '7.1': require('./7_1'),
   '7.0': require('./7_0'),
   '6.8': require('./6_8'),
-  '5.6': require('./5_6'),
-  '7.3': require('./7_3'),
   '7.4': require('./7_4'),
   '7.x': require('./7_x'),
   'master': require('./master')
