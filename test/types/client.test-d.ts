da815b20c7cb981351a5c98777cfea3c7d9546e9
delvedor <tommydelved@gmail.com>
2020-03-16 16:41:56 +0100

    Updated test - Removed old test code - Added tsd dev dependency - Rewritten test with tsd

---
+++ test/types/client.test-d.ts
@@ -0,0 +1,50 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+import { expectType } from 'tsd'
+import { Client, ApiError, ApiResponse, RequestEvent } from '../../'
+
+const client = new Client({
+  node: 'http://localhost:9200'
+})
+
+
+// TODO: rewrite event emitter defintiuon and override on
+client.on('request', (err: ApiError, meta: RequestEvent) => {
+  expectType<ApiError>(err)
+  expectType<RequestEvent>(meta)
+})
+
+client.on('response', (err: ApiError, meta: RequestEvent) => {
+  expectType<ApiError>(err)
+  expectType<RequestEvent>(meta)
+})
+
+// Callbacks style
+{
+  client.info((err, result) => {
+    expectType<ApiError>(err)
+    expectType<ApiResponse>(result)
+  })
+}
+
+// Promise style
+{
+  const promise = client.info()
+  expectType<Promise<ApiResponse>>(promise)
+  promise
+    .then(result => expectType<ApiResponse>(result))
+    .catch((err: ApiError) => expectType<ApiError>(err))
+}
+
+// Promise style with async await
+{
+  const promise = client.info()
+  expectType<Promise<ApiResponse>>(promise)
+  try {
+    expectType<ApiResponse>(await promise)
+  } catch (err) {
+    expectType<any>(err)
+  }
+}
