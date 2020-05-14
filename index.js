11951fe8fc5b77b0d5ee9dfd7d6588eb5bfb31fb
Tomas Della Vedova <delvedor@users.noreply.github.com>
2020-05-14 16:03:08 +0200

    Disable client Helpers in Node.js < 10. (#1194)

--- index.js
+++ index.js
@@ -4,13 +4,16 @@
 
 'use strict'
 
+const nodeMajor = Number(process.versions.node.split('.')[0])
+
 const { EventEmitter } = require('events')
 const { URL } = require('url')
 const debug = require('debug')('elasticsearch')
 const Transport = require('./lib/Transport')
 const Connection = require('./lib/Connection')
 const { ConnectionPool, CloudConnectionPool } = require('./lib/pool')
-const Helpers = require('./lib/Helpers')
+// Helpers works only in Node.js >= 10
+const Helpers = nodeMajor < 10 ? null : require('./lib/Helpers')
 const Serializer = require('./lib/Serializer')
 const errors = require('./lib/errors')
 const { ConfigurationError } = errors
@@ -127,7 +130,9 @@ class Client extends EventEmitter {
       opaqueIdPrefix: options.opaqueIdPrefix
     })
 
-    this.helpers = new Helpers({ client: this, maxRetries: options.maxRetries })
+    if (Helpers !== null) {
+      this.helpers = new Helpers({ client: this, maxRetries: options.maxRetries })
+    }
 
     const apis = buildApi({
       makeRequest: this.transport.request.bind(this.transport),
