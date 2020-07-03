194388194af097731c8bae1d3dd9fa629d3b4a3b
delvedor <tommydelved@gmail.com>
2020-07-03 09:36:11 +0200

    Fix typo

--- api/kibana.d.ts
+++ api/kibana.d.ts
@@ -1,5 +1,4 @@
 // Licensed to Elasticsearch B.V under one or more agreements.
-// ks, so the y
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
 
