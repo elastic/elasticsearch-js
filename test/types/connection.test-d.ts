49a41400f2adf10a851bf33a0e3abbe1aed9c9f8
delvedor <tommydelved@gmail.com>
2020-10-07 17:03:23 +0200

    Updated test

--- test/types/connection.test-d.ts
+++ test/types/connection.test-d.ts
@@ -20,22 +20,35 @@
 import { expectType } from 'tsd'
 import { URL } from 'url'
 import { Connection } from '../../'
+import { ConnectionOptions } from '../../lib/Connection'
 
-const conn = new Connection({
-  url: new URL('http://localhost:9200'),
-  ssl: { ca: 'string' },
-  id: 'id',
-  headers: {},
-  agent: { keepAlive: false },
-  status: 'alive',
-  roles: { master: true },
-  auth: { username: 'username', password: 'password' }
-})
+{
+  const conn = new Connection({
+    url: new URL('http://localhost:9200'),
+    ssl: { ca: 'string' },
+    id: 'id',
+    headers: {},
+    agent: { keepAlive: false },
+    status: 'alive',
+    roles: { master: true },
+    auth: { username: 'username', password: 'password' }
+  })
 
-expectType<Connection>(conn)
-expectType<URL>(conn.url)
-expectType<string>(conn.id)
-expectType<Record<string, any>>(conn.headers)
-expectType<number>(conn.deadCount)
-expectType<number>(conn.resurrectTimeout)
-expectType<string>(conn.status)
+  expectType<Connection>(conn)
+  expectType<URL>(conn.url)
+  expectType<string>(conn.id)
+  expectType<Record<string, any>>(conn.headers)
+  expectType<number>(conn.deadCount)
+  expectType<number>(conn.resurrectTimeout)
+  expectType<string>(conn.status)
+}
+
+{
+  const conn = new Connection({
+    url: new URL('http://localhost:9200'),
+    agent (opts) {
+      expectType<ConnectionOptions>(opts)
+      return 'the agent'
+    }
+  })
+}
