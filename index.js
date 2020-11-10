6c28ee5b339744576074b5f0d05acf4bcba8ff44
delvedor <tommydelved@gmail.com>
2020-11-10 17:38:53 +0100

    Coverage

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ const kEventEmitter = Symbol('elasticsearchjs-event-emitter')
 
 const ESAPI = require('./api')
 
+/* istanbul ignore next */
 if (nodeMajor < 10) {
   process.emitWarning('You are using a version of Node.js that is currently in EOL. ' +
                       'The support for this version will be dropped in 7.12. ' +
@@ -48,6 +49,7 @@ if (nodeMajor < 10) {
   )
 }
 
+/* istanbul ignore next */
 if (nodeMajor >= 10 && nodeMajor < 12) {
   process.emitWarning('You are using a version of Node.js that will reach EOL in April 2021. ' +
                       'The support for this version will be dropped in 7.13. ' +
