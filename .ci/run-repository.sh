564be54a2c382ccf6b6a64c1f41acaf3557d6cc2
delvedor <tommydelved@gmail.com>
2021-09-27 17:13:37 +0200

    Build the code before running integration test

--- .ci/run-repository.sh
+++ .ci/run-repository.sh
@@ -44,4 +44,4 @@ docker run \
   --name elasticsearch-js \
   --rm \
   elastic/elasticsearch-js \
-  node ${run_script_args} test/integration/index.js
+  npm run build && node ${run_script_args} test/integration/index.js
