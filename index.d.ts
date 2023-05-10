7bcf73463a883f30996a6799d435a5c97a9b5583
Grégoire Chauvet <gchauvet.home@gmail.com>
2023-05-10 21:06:47 +0200

    Fix estypesWithBody definition (#1784)

--- index.d.ts
+++ index.d.ts
@@ -22,6 +22,6 @@ import SniffingTransport from './lib/sniffingTransport'
 
 export * from '@elastic/transport'
 export * as estypes from './lib/api/types'
-export * as estypesWithBody from './lib/api/types'
+export * as estypesWithBody from './lib/api/typesWithBodyKey'
 export { Client, SniffingTransport }
 export type { ClientOptions, NodeOptions } from './lib/client'
