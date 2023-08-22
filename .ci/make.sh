8d5e75d795f9df7b470c8e29d18d75204cf7ad35
Josh Mock <joshua.mock@elastic.co>
2023-08-22 10:39:51 -0500

    Fix branch name checker for automatic codegen script (#1980)

--- .ci/make.sh
+++ .ci/make.sh
@@ -65,11 +65,11 @@ case $CMD in
         if [ -v "$VERSION" ] || [[ -z "$VERSION" ]]; then
             # fall back to branch name or `main` if no VERSION is set
             branch_name=$(git rev-parse --abbrev-ref HEAD)
-            if [[ "$branch_name" =~ ^\d+\.\d+ ]]; then
-              echo -e "\033[36;1mTARGET: codegen -> No VERSION found, using branch name: \`$VERSION\`\033[0m"
+            if [[ "$branch_name" =~ ^[0-9]+\.[0-9]+ ]]; then
+              echo -e "\033[36;1mTARGET: codegen -> No VERSION argument found, using branch name: \`$branch_name\`\033[0m"
               VERSION="$branch_name"
             else
-              echo -e "\033[36;1mTARGET: codegen -> No VERSION found, using \`main\`\033[0m"
+              echo -e "\033[36;1mTARGET: codegen -> No VERSION argument found, using \`main\`\033[0m"
               VERSION="main"
             fi
         fi
