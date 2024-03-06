03eaf7ae6e7d4398dace7911ac3dc641db77384f
Elastic Machine <elasticmachine@users.noreply.github.com>
2024-03-06 13:11:28 -0500

    Auto-generated code for 8.12 (#2149)

--- src/api/typesWithBodyKey.ts
+++ src/api/typesWithBodyKey.ts
@@ -11725,7 +11725,7 @@ export interface IngestPipeline {
   on_failure?: IngestProcessorContainer[]
   processors?: IngestProcessorContainer[]
   version?: VersionNumber
-  _meta: Metadata
+  _meta?: Metadata
 }
 
 export interface IngestPipelineConfig {
