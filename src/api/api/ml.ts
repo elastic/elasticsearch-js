a7a864dc50fb368d202f542eb460bee1081bbd22
Elastic Machine <elasticmachine@users.noreply.github.com>
2024-03-19 06:06:38 +0000

    Auto-generated API code

--- src/api/api/ml.ts
+++ src/api/api/ml.ts
@@ -1821,7 +1821,7 @@ export default class Ml {
   async putTrainedModel (this: That, params: T.MlPutTrainedModelRequest | TB.MlPutTrainedModelRequest, options?: TransportRequestOptions): Promise<T.MlPutTrainedModelResponse>
   async putTrainedModel (this: That, params: T.MlPutTrainedModelRequest | TB.MlPutTrainedModelRequest, options?: TransportRequestOptions): Promise<any> {
     const acceptedPath: string[] = ['model_id']
-    const acceptedBody: string[] = ['compressed_definition', 'definition', 'description', 'inference_config', 'input', 'metadata', 'model_type', 'model_size_bytes', 'platform_architecture', 'tags']
+    const acceptedBody: string[] = ['compressed_definition', 'definition', 'description', 'inference_config', 'input', 'metadata', 'model_type', 'model_size_bytes', 'platform_architecture', 'tags', 'prefix_strings']
     const querystring: Record<string, any> = {}
     // @ts-expect-error
     const userBody: any = params?.body
