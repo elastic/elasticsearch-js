42a6fe0f3bcf2cebdcc50fc1ceb9e5bca2175413
Elastic Machine <elasticmachine@users.noreply.github.com>
2025-05-27 18:14:45 +0300

    Auto-generated API code (#2852)

--- src/api/typesWithBodyKey.ts
+++ src/api/typesWithBodyKey.ts
@@ -1768,7 +1768,7 @@ export interface SearchShardProfile {
   index: IndexName
   node_id: NodeId
   searches: SearchSearchProfile[]
-  shard_id: long
+  shard_id: integer
 }
 
 export interface SearchSmoothingModelContainer {
@@ -19767,7 +19767,7 @@ export interface SnapshotSnapshotShardFailure {
   index: IndexName
   node_id?: Id
   reason: string
-  shard_id: Id
+  shard_id: integer
   index_uuid: Id
   status: string
 }
