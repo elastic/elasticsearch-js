87ced2271d8ca809ad074ac37eecb490eb32d9fc
Elastic Machine <elasticmachine@users.noreply.github.com>
2025-07-28 19:06:53 +0200

    Auto-generated API code (#2933)

--- src/api/api/info.ts
+++ src/api/api/info.ts
@@ -39,7 +39,7 @@ import * as TB from '../typesWithBodyKey'
 interface That { transport: Transport }
 
 /**
-  * Get cluster info. Get basic build, version, and cluster information.
+  * Get cluster info. Get basic build, version, and cluster information. ::: In Serverless, this API is retained for backward compatibility only. Some response fields, such as the version number, should be ignored.
   * @see {@link https://www.elastic.co/guide/en/elasticsearch/reference/8.18/rest-api-root.html | Elasticsearch API documentation}
   */
 export default async function InfoApi (this: That, params?: T.InfoRequest | TB.InfoRequest, options?: TransportRequestOptionsWithOutMeta): Promise<T.InfoResponse>
