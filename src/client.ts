609853b0575c1689cc058191b90fb5ccbd418add
Josh Mock <joshua.mock@elastic.co>
2025-08-01 13:26:54 -0500

    Quiet, eslint

--- src/client.ts
+++ src/client.ts
@@ -408,6 +408,7 @@ export default class Client extends API {
       maxResponseSize: options.maxResponseSize,
       maxCompressedResponseSize: options.maxCompressedResponseSize,
       redaction: options.redaction,
+      /* eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error */
       // @ts-ignore enableMetaHeader will be available in transport v9.1.1
       enableMetaHeader: options.enableMetaHeader
     }
