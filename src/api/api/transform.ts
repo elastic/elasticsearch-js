c66766985dee368aa06de5e6714226c329b682ec
Elastic Machine <elasticmachine@users.noreply.github.com>
2025-08-18 17:35:19 +0000

    Auto-generated API code

--- src/api/api/transform.ts
+++ src/api/api/transform.ts
@@ -338,6 +338,35 @@ export default class Transform {
   }
 
   /**
+    * Sets a cluster wide upgrade_mode setting that prepares transform indices for an upgrade.
+    * @see {@link https://www.elastic.co/guide/en/elasticsearch/reference/8.18/transform-set-upgrade-mode.html | Elasticsearch API documentation}
+    */
+  async setUpgradeMode (this: That, params?: T.TODO | TB.TODO, options?: TransportRequestOptionsWithOutMeta): Promise<T.TODO>
+  async setUpgradeMode (this: That, params?: T.TODO | TB.TODO, options?: TransportRequestOptionsWithMeta): Promise<TransportResult<T.TODO, unknown>>
+  async setUpgradeMode (this: That, params?: T.TODO | TB.TODO, options?: TransportRequestOptions): Promise<T.TODO>
+  async setUpgradeMode (this: That, params?: T.TODO | TB.TODO, options?: TransportRequestOptions): Promise<any> {
+    const acceptedPath: string[] = []
+    const querystring: Record<string, any> = {}
+    const body = undefined
+
+    params = params ?? {}
+    for (const key in params) {
+      if (acceptedPath.includes(key)) {
+        continue
+      } else if (key !== 'body') {
+        querystring[key] = params[key]
+      }
+    }
+
+    const method = 'POST'
+    const path = '/_transform/set_upgrade_mode'
+    const meta: TransportRequestMetadata = {
+      name: 'transform.set_upgrade_mode'
+    }
+    return await this.transport.request({ path, method, querystring, body, meta }, options)
+  }
+
+  /**
     * Start a transform. When you start a transform, it creates the destination index if it does not already exist. The `number_of_shards` is set to `1` and the `auto_expand_replicas` is set to `0-1`. If it is a pivot transform, it deduces the mapping definitions for the destination index from the source indices and the transform aggregations. If fields in the destination index are derived from scripts (as in the case of `scripted_metric` or `bucket_script` aggregations), the transform uses dynamic mappings unless an index template exists. If it is a latest transform, it does not deduce mapping definitions; it uses dynamic mappings. To use explicit mappings, create the destination index before you start the transform. Alternatively, you can create an index template, though it does not affect the deduced mappings in a pivot transform. When the transform starts, a series of validations occur to ensure its success. If you deferred validation when you created the transform, they occur when you start the transform—with the exception of privilege checks. When Elasticsearch security features are enabled, the transform remembers which roles the user that created it had at the time of creation and uses those same roles. If those roles do not have the required privileges on the source and destination indices, the transform fails when it attempts unauthorized operations.
     * @see {@link https://www.elastic.co/guide/en/elasticsearch/reference/8.18/start-transform.html | Elasticsearch API documentation}
     */
