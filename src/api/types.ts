aad58005bb9ad0f804831c3c1981c2792024762b
Elastic Machine <elasticmachine@users.noreply.github.com>
2026-06-22 07:50:22 +0000

    Auto-generated API code

--- src/api/types.ts
+++ src/api/types.ts
@@ -14109,7 +14109,7 @@ export interface InferenceJinaAIServiceSettings {
   rate_limit?: InferenceRateLimitSetting
   similarity?: InferenceJinaAISimilarityType
   dimensions?: integer
-  element_type?: InferenceJinaAIElementType
+  embedding_type?: InferenceJinaAIElementType
 }
 
 export type InferenceJinaAIServiceType = 'jinaai'
--- src/api/typesWithBodyKey.ts
+++ src/api/typesWithBodyKey.ts
@@ -14366,7 +14366,7 @@ export interface InferenceJinaAIServiceSettings {
   rate_limit?: InferenceRateLimitSetting
   similarity?: InferenceJinaAISimilarityType
   dimensions?: integer
-  element_type?: InferenceJinaAIElementType
+  embedding_type?: InferenceJinaAIElementType
 }
 
 export type InferenceJinaAIServiceType = 'jinaai'
