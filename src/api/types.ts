8920d3f56c1fe35db988f6522592a59ccd5dd0a6
Elastic Machine <elasticmachine@users.noreply.github.com>
2026-07-20 06:59:37 +0000

    Auto-generated API code

--- src/api/types.ts
+++ src/api/types.ts
@@ -22100,6 +22100,7 @@ export interface TransformGetTransformStatsCheckpointing {
 export interface TransformGetTransformStatsRequest extends RequestBase {
   transform_id: Names
   allow_no_match?: boolean
+  basic?: boolean
   from?: long
   size?: long
   timeout?: Duration
