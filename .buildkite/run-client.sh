d138ee5642f29f5f536936ba790a63f2d55ffe84
margaretjgu <136839162+margaretjgu@users.noreply.github.com>
2026-07-31 11:02:49 -0400

    fix: use VAULT_GITHUB_TOKEN in CI (#3388)

--- .buildkite/run-client.sh
+++ .buildkite/run-client.sh
@@ -15,8 +15,7 @@ docker build \
   --build-arg NODE_VERSION="$NODE_VERSION" \
   .
 
-GITHUB_TOKEN=$(vault read -field=token "$GITHUB_TOKEN_PATH")
-export GITHUB_TOKEN
+export GITHUB_TOKEN="${VAULT_GITHUB_TOKEN}"
 
 echo "--- :javascript: Running tests"
 mkdir -p "$repo/junit-output"
@@ -29,13 +28,14 @@ docker run \
   --env "ELASTIC_PASSWORD=${elastic_password}" \
   --env "ELASTIC_USER=elastic" \
   --env "BUILDKITE=true" \
+  --env BUILDKITE_JOB_ID \
   --volume "/usr/src/app/node_modules" \
   --volume "$repo:/usr/src/app" \
   --volume "$repo/junit-output:/junit-output" \
   --name elasticsearch-js \
   --rm \
   elastic/elasticsearch-js \
-  bash -c "npm run test:integration; TEST_EXIT=\$?; [ -f ./report-junit.xml ] && mv ./report-junit.xml /junit-output/junit-$BUILDKITE_JOB_ID.xml || echo 'No JUnit artifact found'; exit \$TEST_EXIT"
+  bash -c "npm run test:integration-build; BUILD_EXIT=\$?; echo \"=== integration-build exit: \$BUILD_EXIT ===\"; if [ \$BUILD_EXIT -ne 0 ]; then exit \$BUILD_EXIT; fi; ./node_modules/.bin/tap run --jobs=1 --disable-coverage --reporter=junit --reporter-file=report-junit.xml generated-tests/; TAP_EXIT=\$?; echo \"=== tap exit: \$TAP_EXIT ===\"; if [ -f ./report-junit.xml ]; then echo \"JUnit testcases: \$(grep -c '<testcase' ./report-junit.xml || true)\"; echo \"JUnit failures: \$(grep -c '<failure' ./report-junit.xml || true)\"; mv ./report-junit.xml /junit-output/junit-$BUILDKITE_JOB_ID.xml; else echo 'No JUnit artifact found'; fi; exit \$TAP_EXIT"
 
 if [ ! -f "$repo/junit-output/junit-$BUILDKITE_JOB_ID.xml" ]; then
   echo "No JUnit artifact produced, creating empty placeholder"
