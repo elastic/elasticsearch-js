5a16c9875999f44975d0b88299b0d3e8c2201275
Elastic Machine <elasticmachine@users.noreply.github.com>
2026-08-31 15:02:38 +0000

    Auto-generated API code

--- src/api/types.ts
+++ src/api/types.ts
@@ -2512,8 +2512,8 @@ export type InlineGet<TDocument = unknown> = InlineGetKeys<TDocument>
 
 export interface InnerRetriever {
   retriever: RetrieverContainer
-  weight: float
-  normalizer: ScoreNormalizer
+  weight?: float
+  normalizer?: ScoreNormalizer
 }
 
 export type Ip = string
--- src/api/typesWithBodyKey.ts
+++ src/api/typesWithBodyKey.ts
@@ -2590,8 +2590,8 @@ export type InlineGet<TDocument = unknown> = InlineGetKeys<TDocument>
 
 export interface InnerRetriever {
   retriever: RetrieverContainer
-  weight: float
-  normalizer: ScoreNormalizer
+  weight?: float
+  normalizer?: ScoreNormalizer
 }
 
 export type Ip = string
